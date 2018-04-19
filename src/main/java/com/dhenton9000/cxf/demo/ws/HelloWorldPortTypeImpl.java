3ef182f1de94b45a720adad9fba8e179db4e25be
Don Henton <dhenton@amfam.com>
2018-04-19 14:15:39 -0500

    initial commit

---
+++ src/main/java/com/dhenton9000/cxf/demo/ws/HelloWorldPortTypeImpl.java
@@ -0,0 +1,17 @@
+package com.dhenton9000.cxf.demo.ws;
+import javax.jws.WebService;
+import org.apache.contract.helloworld.HelloWorldPortType;
+ 
+
+@WebService(targetNamespace = "http://www.apache.org/contract/HelloWorld",
+    portName="HelloWorldPort",
+    serviceName="HelloWorldService",
+    endpointInterface = "org.apache.contract.helloworld.HelloWorldPortType")
+public class HelloWorldPortTypeImpl implements HelloWorldPortType {
+
+    @Override
+    public String sayHi(String text) {
+        return "Hello " + text;
+    }
+}
+
