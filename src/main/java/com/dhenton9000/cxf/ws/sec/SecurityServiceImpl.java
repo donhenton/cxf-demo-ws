546594aafe9eca085a5e4987eb375791e6b57fb9
Don Henton <dhenton@amfam.com>
2018-04-23 13:05:43 -0500

    adding more services

--- src/main/java/com/dhenton9000/cxf/ws/sec/SecurityServiceImpl.java
+++ src/main/java/com/dhenton9000/cxf/ws/sec/SecurityServiceImpl.java
@@ -1,11 +1,18 @@
 package com.dhenton9000.cxf.ws.sec;
 
 import com.dhenton9000.birt.jpa.domain.security.Applications;
+import com.dhenton9000.birt.jpa.domain.security.Groups;
+import com.dhenton9000.birt.jpa.domain.security.Users;
 import com.dhenton9000.birt.jpa.service.security.ApplicationsService;
+import com.dhenton9000.birt.jpa.service.security.GroupsService;
 import com.dhenton9000.birt.jpa.service.security.UsersService;
 import com.dhenton9000.sec.contract.securityservice.SecurityServiceWSDL;
 import com.dhenton9000.sec.schema.securityservice.ApplicationsListType;
 import com.dhenton9000.sec.schema.securityservice.ApplicationsType;
+import com.dhenton9000.sec.schema.securityservice.GroupsListType;
+import com.dhenton9000.sec.schema.securityservice.GroupsType;
+import com.dhenton9000.sec.schema.securityservice.UsersListType;
+import com.dhenton9000.sec.schema.securityservice.UsersType;
 import java.math.BigInteger;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +25,9 @@ public class SecurityServiceImpl implements SecurityServiceWSDL {
     @Autowired
     ApplicationsService applicationsService;
 
+    @Autowired
+    GroupsService groupsService;
+
     @Override
     public ApplicationsListType getApplications() {
 
@@ -28,11 +38,58 @@ public class SecurityServiceImpl implements SecurityServiceWSDL {
             ApplicationsType e = new ApplicationsType();
             e.setApplicationName(a.getApplicationName());
             e.setId(BigInteger.valueOf(a.getId()));
-            
+
             appList.getApplications().add(e);
-        
+
         });
         return appList;
     }
 
+    @Override
+    public UsersListType getUsers() {
+
+        UsersListType userList = new UsersListType();
+
+        List<Users> userItems = usersService.findAll();
+        userItems.forEach((Users a) -> {
+            UsersType e = new UsersType();
+            e.setLogin(a.getLogin());
+            e.setUsername(a.getUsername());
+            e.setId(BigInteger.valueOf(a.getUserid()));
+            userList.getUsers().add(e);
+        });
+        return userList;
+    }
+
+    @Override
+    public GroupsListType getGroups() {
+        GroupsListType groupList = new GroupsListType();
+
+        List<Groups> groupItems = groupsService.findAll();
+        groupItems.forEach(a -> {
+            GroupsType e = new GroupsType();
+            e.setGroupName(a.getGroupName());
+            e.setId(BigInteger.valueOf(a.getId()));
+
+            groupList.getGroups().add(e);
+
+        });
+        return groupList;
+    }
+
 }
+
+
+/*
+
+soap ui payload to call the groups function
+
+<soapenv:Envelope xmlns:ns2="http://dhenton9000.com/sec/schema/SecurityService" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+   <soapenv:Header/>
+   <soapenv:Body>
+	<ns2:GetGroups />
+   </soapenv:Body>
+</soapenv:Envelope>
+
+
+*/
